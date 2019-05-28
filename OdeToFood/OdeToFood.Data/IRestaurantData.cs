ef36f619a8223074d757f752417c21c4ed9be0a1
Adam Storr <astorr@gmail.com>
2019-05-28 20:17:22 +0100

    Splitting out the repository to allow for readonly only and only that is decorated to be cached (single example).

--- OdeToFood/OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -1,17 +1,13 @@
 ﻿using OdeToFood.Core;
-using System.Collections.Generic;
 
 
 namespace OdeToFood.Data
 {
-    public interface IRestaurantData
+    public interface IRestaurantData : IReadOnlyRestaurantData
     {
-        IEnumerable<Restaurant> GetRestaurantsByName(string name);
-        Restaurant GetById(int id);
         Restaurant Update(Restaurant updatedRestaurant);
         Restaurant Add(Restaurant newRestaurant);
         Restaurant Delete(int id);
-        int GetCountOfRestaurants();
         int Commit();
     }
 }
